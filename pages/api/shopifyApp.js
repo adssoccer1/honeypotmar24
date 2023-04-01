f3eb7e91b3cba0883b8075b4de14cf625b6d48be
Aidan <adssoccer1@gmail.com>
2023-03-31 22:19:25 -0400

    add shopify install db logic

--- pages/api/shopifyApp.js
+++ pages/api/shopifyApp.js
@@ -1,8 +1,13 @@
 // pages/api/shopifyApp.js
+// pages/api/shopifyApp.js
+
+import { ref, get, update, orderByChild, equalTo, push, query, set } from 'firebase/database';
+import { db } from '../../lib/firebase';
 
 export default async function handler(req, res) {
     console.log("shopify install/unistall endpoint hit");
     if (req.method === 'POST') {
+        console.log(req.body);
       const { shopUrl, action } = req.body;
       console.log("shopify install/unistall endpoint url", shopUrl, " and action ", action);
 
@@ -38,13 +43,51 @@ export default async function handler(req, res) {
     }
   }
   
-  async function handleInstall(shopUrl) {
+
+async function handleInstall(shopUrl) {
     // Add your logic for handling the installation event
     console.log(`Handling installation for store: ${shopUrl}`);
-  }
+    // Check and update advertiser users
+    const usersRef = ref(db, 'users');
+    const usersQuery = query(usersRef, orderByChild('shopUrl'), equalTo(shopUrl));
+    
+    const usersSnapshot = await get(usersQuery);
+    console.log('usersSnapshot:');
+
+    if (usersSnapshot.exists()) {
+        usersSnapshot.forEach((userSnapshot) => {
+        const userId = userSnapshot.key;
+        update(ref(db, `users/${userId}`), { verifiedUrl: false });
+        });
+    }
+    
+    // Check and update verifiedShops
+    const verifiedShopsRef = ref(db, 'verifiedShops');
+    const verifiedShopsQuery = query(verifiedShopsRef, orderByChild('shopUrl'), equalTo(shopUrl));
+    
+    const verifiedShopsSnapshot = await get(verifiedShopsQuery);
+    
+    if (verifiedShopsSnapshot.exists()) {
+        verifiedShopsSnapshot.forEach((shopSnapshot) => {
+        const shopId = shopSnapshot.key;
+        update(ref(db, `verifiedShops/${shopId}`), { installed: true });
+        });
+    } else {
+        // Create a new entry in verifiedShops
+        const newShopId = push(ref(db, 'verifiedShops')).key;
+        const newShop = {
+        shopUrl: shopUrl,
+        verifiedUrl: false,
+        installed: true,
+        };
+        await set(ref(db, `verifiedShops/${newShopId}`), newShop);
+    }
+    }
+
+      
   
   async function handleUninstall(shopUrl) {
     // Add your logic for handling the uninstallation event
-    console.log(`Handling uninstallation for store: ${shopifyStoreUrl}`);
+    console.log(`Handling uninstallation for store: ${shopUrl}`);
   }
   
