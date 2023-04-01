4c4bc40fa56723fe041b64a378a04d6dae3a5ecf
Aidan <adssoccer1@gmail.com>
2023-03-31 20:33:12 -0400

    add shopify install uninstall endpoint

---
+++ pages/api/shopifyApp.js
@@ -0,0 +1,50 @@
+// pages/api/shopifyApp.js
+
+export default async function handler(req, res) {
+    console.log("shopify install/unistall endpoint hit");
+    if (req.method === 'POST') {
+      const { shopifyStoreUrl, action } = req.body;
+      console.log("shopify install/unistall endpoint url", shopifyStoreUrl, " and action ", action);
+
+      if (!shopifyStoreUrl || !action) {
+        res.status(400).json({ error: 'Invalid request. Both shopifyStoreUrl and action are required.' });
+        return;
+      }
+  
+      try {
+        switch (action) {
+          case 'install':
+            // Handle the installation event
+            await handleInstall(shopifyStoreUrl);
+            res.status(200).json({ message: 'Installation event processed successfully.' });
+            break;
+  
+          case 'uninstall':
+            // Handle the uninstallation event
+            await handleUninstall(shopifyStoreUrl);
+            res.status(200).json({ message: 'Uninstallation event processed successfully.' });
+            break;
+  
+          default:
+            res.status(400).json({ error: 'Invalid action. Action must be either "install" or "uninstall".' });
+            break;
+        }
+      } catch (error) {
+        console.error('Error processing the event:', error);
+        res.status(500).json({ error: 'An error occurred while processing the event.' });
+      }
+    } else {
+      res.status(405).json({ error: 'Method not allowed. Please use POST.' });
+    }
+  }
+  
+  async function handleInstall(shopifyStoreUrl) {
+    // Add your logic for handling the installation event
+    console.log(`Handling installation for store: ${shopifyStoreUrl}`);
+  }
+  
+  async function handleUninstall(shopifyStoreUrl) {
+    // Add your logic for handling the uninstallation event
+    console.log(`Handling uninstallation for store: ${shopifyStoreUrl}`);
+  }
+  
