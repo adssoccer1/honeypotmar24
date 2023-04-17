65ebe356d25032aba0c084f550d85581c43978cb
Aidan <adssoccer1@gmail.com>
2023-04-17 00:26:17 -0400

    minor details

--- components/leaderboard/Board.js
+++ components/leaderboard/Board.js
@@ -9,7 +9,7 @@ import Link from 'next/link';
         <div className="mt-8 flow-root">
           <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-12 align-middle sm:px-6 lg:px-8">
-              <table className="min-w-full divide-y divide-gray-300">
+              <table className="min-w-full text-left divide-y divide-gray-300">
                 <thead>
                   <tr>
                     <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
@@ -30,40 +30,37 @@ import Link from 'next/link';
                   </tr>
                 </thead>
                 <tbody className="divide-y divide-gray-200 bg-white">
-                  {deals.map((deal) => (
-                    <tr key={deal.id}>
-                      <td className="whitespace-nowrap py-9 pl-4 pr-3 text-sm sm:pl-0">
-                        <div className="flex items-center">
-                          <div className="h-11 w-11 flex-shrink-0">
-                            <img className="h-11 w-11 rounded-full" src={deal.image} alt="" />
-                          </div>
-                          <div className="ml-4">
-                            <div className="font-medium text-gray-900">{deal.shopurl}</div>
-                            <div className="mt-1 text-gray-500">{deal.ownerEmail}</div>
-                          </div>
-                        </div>
-                      </td>
-                      <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                        <div className="text-gray-900">{deal.title}</div>
-                      </td>
-                      <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                        <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
-                          Available
-                        </span>
-                      </td>
-                      <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">{deal.commission}%</td>
-                      <td className="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-
-                        <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                    {deals.map((deal) => (
+                        <tr key={deal.id}>
+                        <td className="whitespace-nowrap py-9 pr-3 text-sm sm:pl-0">
+                            <div className="flex items-center">
+                            
+                            <div className="ml-4">
+                                <div className="font-medium text-gray-900">{deal.shopurl}</div>
+                                <div className="mt-1 text-gray-500">{deal.ownerEmail}</div>
+                            </div>
+                            </div>
+                        </td>
+                        <td className="whitespace-nowrap py-5 px-3 text-sm text-gray-500">
+                            <div className="text-gray-900">{deal.title}</div>
+                        </td>
+                        <td className="whitespace-nowrap py-5 px-3 text-sm text-gray-500">
+                            <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
+                            Available
+                            </span>
+                        </td>
+                        <td className="whitespace-nowrap py-5 px-3 text-sm text-gray-500">{deal.commission}%</td>
+                        <td className="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                            <a href="#" className="text-indigo-600 hover:text-indigo-900">
                             {(user && user.accountType === 'newsletter'
-                            ? <ClaimOffer key={deal.id} deal={deal} user={user} />
-                            : <Link href="/register">Claim Offer</Link>
+                                ? <ClaimOffer key={deal.id} deal={deal} user={user} />
+                                : <Link href="/register">Claim Offer</Link>
                             )}
                             <span className="sr-only">, {deal.name}</span>
-                        </a>
-                      </td>
-                    </tr>
-                  ))}
+                            </a>
+                        </td>
+                        </tr>
+                    ))}
                 </tbody>
               </table>
             </div>
--- components/leaderboard/SubmitDealForm.js
+++ components/leaderboard/SubmitDealForm.js
@@ -151,7 +151,7 @@ const SubmitDealForm = ({user, closeModal}) => {
       </div>
 
       <div className="mt-6 flex items-center justify-end gap-x-6">
-        <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+        <button onClick={closeModal} type="button" className="text-sm font-semibold leading-6 text-gray-900">
           Cancel
         </button>
         <button
